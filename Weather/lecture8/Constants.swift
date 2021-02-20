2dc318db1b6c424781efef95b0120277e17e2794
AJumaseit <57934149+AJumaseit@users.noreply.github.com>
2021-02-20 22:18:47 +0600

    Add files via upload

---
+++ Weather/lecture8/Constants.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Constants {
+    static let host = "https://api.openweathermap.org/data/2.5/onecall?lat=51.1801&lon=71.446&exclude=minutely,alerts&appid=0e9477e67e53c8c91844f7d87860ae02&units=metric"
+    static let city = "Astana"
+    static let apiKey = ""
+    
+    static var lon = "71.446"
+    static var lat = "51.1801"
+}
